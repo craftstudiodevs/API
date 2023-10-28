2438c5f88c575133694fc060626e35645537f326
isXander <xandersmith2008@gmail.com>
2023-10-28 20:01:06 +0100

    Fix CORS

--- src/main/kotlin/dev/craftstudio/plugins/CORS.kt
+++ src/main/kotlin/dev/craftstudio/plugins/CORS.kt
@@ -2,30 +2,14 @@ package dev.craftstudio.plugins
 
 import io.ktor.http.*
 import io.ktor.server.application.*
-import io.ktor.server.application.*
 import io.ktor.server.plugins.cors.routing.*
 
 fun Application.configureCORS() {
     install(CORS) {
-        HttpMethod.DefaultMethods.forEach(::allowMethod)
-        listOf(
-            HttpHeaders.AccessControlAllowOrigin,
-            HttpHeaders.AccessControlAllowHeaders,
-            HttpHeaders.AccessControlAllowMethods,
-            HttpHeaders.AccessControlAllowCredentials,
-            HttpHeaders.AccessControlMaxAge,
-            HttpHeaders.AccessControlRequestHeaders,
-            HttpHeaders.AccessControlRequestMethod,
-            HttpHeaders.AccessControlExposeHeaders,
-            HttpHeaders.Authorization,
-        ).forEach {
-            exposeHeader(it)
-            allowHeader(it)
-        }
+        allowHeader(HttpHeaders.ContentType)
+        allowHeader(HttpHeaders.Authorization)
+        allowHeader(HttpHeaders.Accept)
+        allowHeader(HttpHeaders.AcceptLanguage)
         allowHost("localhost:3000")
-        anyHost()
-        allowCredentials = true
-        allowSameOrigin = true
-        allowOrigins { true }
     }
 }
